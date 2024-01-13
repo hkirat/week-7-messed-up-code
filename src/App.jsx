f5fe0acd50aa03516f76f21b97ea87cc497df0bc
karananeja <karananeja.r@gmail.com>
2024-01-13 20:31:24 +0530

    added the suspense api and created a simple loader comp for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,60 @@
-
-import { lazy } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import './App.css'
-
-const Dashboard = lazy(() => import('./components/Dashboard'))
-const Landing = lazy(() => import('./components/Landing'))
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+const Dashboard = lazy(() => import('./components/Dashboard'));
+const Landing = lazy(() => import('./components/Landing'));
 
 function App() {
-  
   // Suspense API
   return (
-    <div>
+    <>
       <BrowserRouter>
         <Appbar />
         <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/" element={<Landing />} />
+          <Route
+            path='/dashboard'
+            element={
+              <Suspense fallback={<Loader />}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/'
+            element={
+              <Suspense fallback={<Loader />}>
+                <Landing />
+              </Suspense>
+            }
+          />
         </Routes>
       </BrowserRouter>
+    </>
+  );
+}
+
+function Loader() {
+  return (
+    <div className='loader'>
+      <div className='spinner-top' />
+      <div className='spinner-bottom' />
     </div>
-  )
+  );
 }
 
 function Appbar() {
   const navigate = useNavigate();
 
-  return <div>
-      <div>
-        <button onClick={() => {
-          navigate("/");
-        }}>Landing page</button>
-
-        <button onClick={() => {
-          navigate("/dashboard");
-        }}>Dashboard</button>
-
-      </div>
-  </div>
+  return (
+    <>
+      <button className='btn' onClick={() => navigate('/')}>
+        Landing page
+      </button>
+      <button className='btn' onClick={() => navigate('/dashboard')}>
+        Dashboard
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
