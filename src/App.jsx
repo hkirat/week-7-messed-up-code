04f75e646dac7ec09752f02c91bb075302e53155
SayantanBar <121619214+SayantanBar@users.noreply.github.com>
2024-01-13 20:02:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { lazy } from "react";
+import { lazy ,Suspense} from "react";
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 
@@ -13,10 +13,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Appbar />
+        <Suspense fallback={<div>Loading...</div>}>
         <Routes>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/" element={<Landing />} />
         </Routes>
+          <Suspense/>
       </BrowserRouter>
     </div>
   )
